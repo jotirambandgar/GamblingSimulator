41b535d176b0027fd6eb0d3e8d5c189b92189db9
jotirambandgar <bandgar09@gmail.com>
2019-12-11 12:58:31 +0530

    Added : Gambling playing limit for day

--- gambling.sh
+++ gambling.sh
@@ -1,20 +1,26 @@
 #!/bin/bash -x
 
 echo "Welcome to Gambling Simulator"
-STAKE=100
+stake=100
 BET=1
 
+function gamblingForDay(){
+	lossLimit=$(( $stake / 2 ))
+	winLimit=$(( $stake + $lossLimit ))
+	while [[ $stake -gt $lossLimit ]] && [[ $stake -lt $winLimit  ]]
+	do
+		if [ $((RANDOM%2)) -eq 1 ]
+  		then
+			stake=$(( $stake + $BET ))
+		 else
+			stake=$(( $stake - $BET ))
+
+   	fi
+	done
+	echo "$stake"
+} 
+
 function main(){
-	if [ $((RANDOM%2)) -eq 1 ]
-
-	then
-
-		echo " win"
-
-	else
-
-		echo "loss"
-
-	fi
+		gamblingForDay
 }
 main
