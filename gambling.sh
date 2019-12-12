b8ecbf27e94eb6d11124511e61c4d5dc6ed6c1e3
jotirambandgar <bandgar09@gmail.com>
2019-12-12 16:36:38 +0530

    Added : find luckiest and unluckiestDay

--- gambling.sh
+++ gambling.sh
@@ -5,7 +5,11 @@ stake=100
 BET=1
 win=0
 loss=0
+declare -A gamblingRecord;
 function gamblingForDay(){
+	stake=100
+	win=0
+	loss=0
 	lossLimit=$(( $stake / 2 ))
 	winLimit=$(( $stake + $lossLimit ))
 	while [[ $stake -gt $lossLimit ]] && [[ $stake -lt $winLimit  ]]
@@ -14,21 +18,42 @@ function gamblingForDay(){
   		then
 			stake=$(( $stake + $BET ))
 			((win++))
+
 		 else
 			stake=$(( $stake - $BET ))
-			((loss++))
+			((loss--))
+
    	fi
 	done
-	echo "$stake"
-} 
+
+	if [ $stake -eq 50 ]
+	then
+		echo "-50"
+	else
+		echo "50"
+	fi
+
+}
 
 function main(){
+	stakeRecord=0
+	unluckiestDay=""
 	for (( i=0; i<20 ; i++ ))
 	do
-		gamblingForDay
+		echo "stake for day $i"
+		dayAmount=$(gamblingForDay)
+    	stakeRecord=$(( $stakeRecord + $dayAmount ))
+		#echo "day1   stake total-->$stakeRecord     $dayAmount"
+		gamblingRecord["day$i"]="$dayAmount $stakeRecord"
 	done
-	echo "winning amount for 20 days : $win"
-	echo "loss amount for 20 days : $loss"
+		echo ${gamblingRecord[@]}
+
+	 #for key in ${!gamblingRecord[@]}
+  	for(( i=0; i<20 ; i++ )) 
+	do
+     
+      echo "day$i" ${gamblingRecord["day$i"]}
+  	 done | sort -k3 -nr | awk 'NR==20{ print "unluckiestDay " $0 } AND NR==1{ print "luckiestDay " $0 }'
 
 }
 main
